4110d4d9346e2331fe4d2ed78216f12ae9dd2160
Thijs Boom <twboom@hotmail.nl>
2021-01-28 01:47:20 +0100

    Almost made the cheapest seller

--- src/sim/market.js
+++ src/sim/market.js
@@ -9,7 +9,9 @@ class person {
         this.randomWealth = 1 + Math.random() * 10;
         this.products = Math.floor(this.randomWealth) * Math.floor(Math.random() * 10) + 1;
         this.money = 10 * this.randomWealth * Math.random() * 10;
-        this.productCost = (this.money / this.products) * Math.random()
+        this.productCost = (this.money / this.products) * Math.random();
+        this.status = {'bought': false, 'sold': true};
+        this.statistics = {'spend': 0, 'earned': 0}
     };
 };
 
@@ -26,4 +28,11 @@ function market() {
     for (let i = 0; i < marketEnv.persons.length; i++) {
 
     };
+}
+
+function cheapestSeller() {
+    const prices = marketEnv.persons.map(({ productCost }) => productCost);
+    prices.sort();
+    const seller = marketEnv.persons.find( ({ productCost }) => productCost == prices[0])
+    return seller.index
 }
